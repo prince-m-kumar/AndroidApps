ded78fd06b9e947584b88b4b633d546f72ea756b
Prince Kumar <prince.m.singh11@gmail.com>
2018-03-21 12:58:52 +0530

    ChatBot App Init: Using Api.ai try to build chatbot

---
+++ ChatBot/app/src/main/java/kumar/prince/chatbot/MainActivity1.kt
@@ -0,0 +1,12 @@
+package kumar.prince.chatbot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
